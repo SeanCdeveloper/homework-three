d43bc82a44f9458157366f4e7dcd68938ec7cb9d
Sean Cumming <seancumming@Seans-MacBook-Pro.local>
2019-11-30 09:40:42 -0500

    External JavaScript File + External CSS File Added

---
+++ Assets/JavaScript/script.js
@@ -0,0 +1,34 @@
+document.getElementById("generateBtn").addEventListener("click", generate);
+
+document.getElementById("copyBtn").addEventListener("click", copyPassword);
+
+ /* Generate Random Password */
+
+ function generate(event) {
+    var pwRange = prompt("How Many Characters Do You Want In Your Password?");
+
+    // Set Password Length as Value of prompt() Method
+    var pwLength = pwRange;
+
+    // Variables Containing Possible Password Values To Use For Password
+    var values = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+
+    // Open-Quotation Value for Storing Generated Password
+    var password = "";
+
+    // For Loop Choosing Password Characters Based On Selected Length
+    for (var i = 0; i < pwLength; i++) {
+
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+    }
+    // Adding PassWord to input#display Object
+    alert(pwRange);
+    document.getElementById("display").value = password;
+}
+
+/* Copy to Clipboard Function */
+function copyPassword(event) {
+    document.getElementById("display").select();
+    document.execCommand("Copy");
+    alert("Password Copied to ClipBoard");
+}
